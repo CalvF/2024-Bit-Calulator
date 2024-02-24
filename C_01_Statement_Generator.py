527f8b8984fc238836a7199bf6264e5cc4123c7e
CalvF <110645028+CalvF@users.noreply.github.com>
2024-02-24 22:42:14 +1300

    Add files via upload

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,8 @@
+#Function go here
+def statement_generator(statement, decoration):
+    print(f"{decoration * 5} {statement} {decoration * 5}")
+
+
+#main routine goes here
+statement_generator ("Instruction",  "'")
+
